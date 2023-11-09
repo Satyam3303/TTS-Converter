670cf8a1b5ab9749f0e85105cbdf623410fdb626
SHIVAM SATYAM <78131073+Satyam3303@users.noreply.github.com>
2023-11-09 13:04:23 +0530

    1st Commit

---
+++ tts/src/App.js
@@ -0,0 +1,41 @@
+import './App.css';
+import { useSpeechSynthesis } from 'react-speech-kit';
+import { useState } from 'react';
+
+function App() {
+  const [text, setText] = useState('');
+  const { speak, voices } = useSpeechSynthesis();
+  const [selectedVoice, setSelectedVoice] = useState(null);
+
+  const handleClick = () => {
+    speak({ text, voice: selectedVoice });
+  }
+
+  const handleVoiceChange = (e) => {
+    const selectedVoiceName = e.target.value;
+    const selectedVoice = voices.find(voice => voice.name === selectedVoiceName);
+    setSelectedVoice(selectedVoice);
+  }
+
+  return (
+    <>
+      <div className='App'>
+        <div>
+          <h1 className='App-header'>TTS CONVERTER</h1>
+          <textarea className="textAreaStyle" onChange={(e) => { setText(e.target.value) }}></textarea>
+        </div>
+        <div>
+          <button className="buttonStyle" onClick={() => { handleClick() }}>Listen</button>
+          <select className="buttonStyle" onChange={(e) => { handleVoiceChange(e) }}>
+            {voices.map((voice, index) => (
+              <option key={index} value={voice.name}>{voice.name}</option>
+            ))}
+          </select>
+        </div>
+        <footer className='footer'>Made by Shivam</footer>
+      </div>
+    </>
+  );
+}
+
+export default App;
